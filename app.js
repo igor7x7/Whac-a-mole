123a8e37ea7327594a6508fafb554c2bf89bbbbd
igor7x7 <https://github.com/igor7x7>
2022-03-10 10:17:51 -0300

    correção no nome da toupeira

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
 const quadrados = document.querySelectorAll(".quadrado")
-const topeira = document.querySelector(".topeira")
+const toupeira = document.querySelector(".toupeira")
 const pontuacao = document.querySelector("#pontuacao")
 const tempoRestante = document.querySelector("#tempo-restante")
 
 let resultado = 0
 let posicaoHit
-let tempoPercorrido = 10
+let tempoPercorrido = 20
 let timerId = null
 
 function randomQuadrado() {
-    quadrados.forEach(quadrado => quadrado.classList.remove("topeira"))
+    quadrados.forEach(quadrado => quadrado.classList.remove("toupeira"))
     let randomQuadrado = quadrados[Math.floor(Math.random() * 9)]
-    randomQuadrado.classList.add('topeira')
+    randomQuadrado.classList.add('toupeira')
 
     posicaoHit = randomQuadrado.id
 }
@@ -27,11 +27,11 @@ quadrados.forEach(quadrado => {
     })
 })
 
-function movimentoTopeira() {
+function movimentoToupeira() {
     timerId = setInterval(randomQuadrado, 500)
 }
 
-movimentoTopeira()
+movimentoToupeira()
 
 function contagemRegressiva() {
     tempoPercorrido--
